aa436726308e408fbf0d92d9af15517d50738423
AwakexD <aleks.parvanow@gmail.com>
2024-01-26 21:48:11 +0200

    Initial commit

---
+++ C# Web Basics/02.Web Server - Asynchronous Processing/PrintPrimeCount/PrintPrimeCount/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+
+namespace PrintPrimeCount
+{
+    class Program
+    {
+        static int Count = 0;
+        static object lockObj = new object();
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Thread thread = new Thread(() => PrintPrimeCount(1, 2_500_000));
+            thread.Name = "1";
+            thread.Start();
+            Thread thread2 = new Thread(() => PrintPrimeCount(2_500_001, 5_000_000));
+            thread2.Start();
+            thread2.Name = "2";
+            Thread thread3 = new Thread(() => PrintPrimeCount(5_000_001, 7_500_000));
+            thread3.Start();
+            thread3.Name = "3";
+            Thread thread4 = new Thread(() => PrintPrimeCount(7_500_001, 10_000_000));
+            thread4.Start();
+            thread4.Name = "4";
+
+            thread.Join();
+            thread2.Join();
+            thread3.Join();
+            thread4.Join();
+
+            Console.WriteLine(Count);
+            Console.WriteLine(sw.Elapsed);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(input.ToUpper());
+            }
+        }
+
+        static void PrintPrimeCount(int min, int max )
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            int n = 100000000;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool isPrime  = true;
+                for (int j = 2; j < Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    lock (lockObj)
+                    {
+                        Count++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Web Basics/02.Web Server - Asynchronous Processing/PrintPrimeCount/SynchronousUiProblem/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SynchronousUiProblem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowImage(Image1, "https://http.cat/images/500.jpg");
+            ShowImage(Image2, "https://http.cat/images/400.jpg");
+            ShowImage(Image3, "https://http.cat/images/404.jpg");
+            ShowImage(Image4, "https://http.cat/images/200.jpg");
+            ShowImage(Image5, "https://http.cat/images/302.jpg");
+            ShowImage(Image6, "https://http.cat/images/503.jpg");
+        }
+
+        private async Task ShowImage(Image image, string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = httpClient.GetAsync(url).GetAwaiter().GetResult();
+            byte[] imageBytes = response.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+            image.Source = LoadImage(imageBytes);
+        }
+
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+    }
+}
