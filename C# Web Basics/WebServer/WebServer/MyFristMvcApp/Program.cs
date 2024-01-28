b416eb6398917f74440a318c809ec5ade69fcba1
AwakexD <aleks.parvanow@gmail.com>
2024-01-28 22:43:58 +0200

    Create WebServer project architecture

---
+++ C# Web Basics/WebServer/WebServer/MyFristMvcApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using WebServer.HTTP;
+
+namespace MyFristMvcApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer server = new HttpServer();
+            server.Start(80);
+
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/login", Login);
+            server.AddRoute("/about", About);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        static HttpResponse Login(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        static HttpResponse About(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C# Web Basics/WebServer/WebServer/WebServer.HTTP/IHttpServer.cs
@@ -0,0 +1,9 @@
+﻿namespace WebServer.HTTP
+{
+    public interface IHttpServer
+    {
+        void Start(int port);
+
+        void AddRoute(string path, Func<HttpRequest, HttpResponse> action);
+    }
+}
