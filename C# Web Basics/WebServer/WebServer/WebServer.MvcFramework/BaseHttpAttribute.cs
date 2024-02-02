badde692070a365132692ee33a41a35a88f20711
AwakexD <aleks.parvanow@gmail.com>
2024-02-02 13:15:46 +0200

    Delete project and xunit tests are not working

---
+++ C# Web Basics/WebServer/WebServer/WebServer.MvcFramework/BaseHttpAttribute.cs
@@ -0,0 +1,12 @@
+﻿using WebServer.HTTP;
+using System;
+
+namespace SUS.MvcFramework
+{
+    public abstract class BaseHttpAttribute : Attribute
+    {
+        public string Url { get; set; }
+
+        public abstract WebServer.HTTP.HttpMethod Method { get; }
+    }
+}
