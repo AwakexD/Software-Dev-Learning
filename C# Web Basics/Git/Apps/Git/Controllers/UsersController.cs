200d709fe9841e987388f3b07b1a34487d4835e8
AwakexD <aleks.parvanow@gmail.com>
2024-02-04 21:48:59 +0200

    Implemented fully UsersController and UsersService

--- C# Web Basics/Git/Apps/Git/Controllers/UsersController.cs
+++ C# Web Basics/Git/Apps/Git/Controllers/UsersController.cs
@@ -11,7 +11,6 @@ namespace Git.Controllers
     using SUS.HTTP;
     using SUS.MvcFramework;
 
-    //ToDo : Login and Register
     public class UsersController : Controller
     {
         private readonly IUsersService usersService;
@@ -23,29 +22,88 @@ namespace Git.Controllers
 
         public HttpResponse Login()
         {
+            if (this.IsUserSignedIn() == true)
+            {
+                return this.Error("Only logged out users can login.");
+            }
             return this.View();
         }
 
         [HttpPost]
         public HttpResponse Login(LoginInputModel loginModel)
         {
-            return this.View();
+            if (usersService.DoesUserExist(loginModel.Username, loginModel.Password) == false)
+            {
+                return this.Error("User with this username and password does not exist.");
+            }
+
+            var userId = usersService.GetUserId(loginModel.Username, loginModel.Password);
+            this.SignIn(userId);
+            return this.Redirect("/");
         }
 
         public HttpResponse Register()
         {
+            if (this.IsUserSignedIn() == true)
+            {
+                return this.Error("Only logged out users can register.");
+            }
+
             return this.View();
         }
 
         [HttpPost]
         public HttpResponse Register(RegisterInputModel registerModel)
         {
-            return this.View();
+
+            if (string.IsNullOrEmpty(registerModel.Username) || registerModel.Username.Length < 5 && registerModel.Username.Length > 20)
+            {
+                return this.Error("Username should be between 5 and 20 characters long.");
+            }
+
+            if (usersService.IsEmailValid(registerModel.Email) == false || string.IsNullOrEmpty(registerModel.Email))
+            {
+                return this.Error("Invalid email!");
+            }
+
+            if (string.IsNullOrEmpty(registerModel.Password) || registerModel.Password.Length < 6 || registerModel.Password.Length > 20)
+            {
+                return this.Error("Password should be between 6 and 20 characters long.");
+            }
+            if (this.IsUserSignedIn())
+            {
+                return this.Error("Only logged off users can register");
+            }
+
+            if (usersService.IsEmailAvailable(registerModel.Email) == false)
+            {
+                return this.Error("Email is already taken.");
+            }
+
+            if (usersService.IsUsernameAvailable(registerModel.Username) == false)
+            {
+                return this.Error("Username is already taken");
+            }
+
+            if (usersService.DoesUserExist(registerModel.Username, registerModel.Password))
+            {
+                return this.Error("User already exists.");
+            }
+
+            this.usersService.CreateUser(registerModel.Username, registerModel.Email, registerModel.Password);
+
+            return this.Redirect("/Users/Login");
         }
 
         public HttpResponse Logout()
         {
-            return this.View();
+            if (this.IsUserSignedIn() == false)
+            {
+                return this.Error("Only logged in users can logout.");
+            }
+
+            this.SignOut();
+            return this.Redirect("/");
         }
     }
 }
