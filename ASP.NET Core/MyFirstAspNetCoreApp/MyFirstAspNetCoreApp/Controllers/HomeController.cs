cce7a9eb4c330429c2e23d118278b498e367c135
AwakexD <aleks.parvanow@gmail.com>
2024-02-11 20:42:09 +0200

    Init ASP.NET Core

---
+++ ASP.NET Core/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstAspNetCoreApp.Models;
+using System.Diagnostics;
+using MyFirstAspNetCoreApp.Data;
+using MyFirstAspNetCoreApp.ViewModels.Home;
+
+namespace MyFirstAspNetCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {  
+            IndexViewModel viewModel = new IndexViewModel()
+            {
+                UsersCount = this.dbContext.Users.Count(),
+                ProcessorsCount = Environment.ProcessorCount,
+                CurrentDate = DateTime.Now.ToUniversalTime(),
+            };
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET Core/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstAspNetCoreApp.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public int UsersCount { get; set; }
+
+        public int ProcessorsCount { get; set; }
+
+        public DateTime CurrentDate { get; set; }
+    }
+}
