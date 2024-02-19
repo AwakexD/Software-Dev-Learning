1504e1474697202a31608453ee6a679c623c79dd
AwakexD <aleks.parvanow@gmail.com>
2024-02-19 22:08:37 +0200

    test data binding

---
+++ ASP.NET Core/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/Controllers/RecipesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAspNetCoreApp.Controllers;
+
+public class AddRecipeInputModel
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CookingDuration { get; set; }
+}
+
+public class RecipesController : Controller
+{
+    public IActionResult Add(AddRecipeInputModel inputModel)
+    {
+        return this.Json(inputModel);
+    }
+}
