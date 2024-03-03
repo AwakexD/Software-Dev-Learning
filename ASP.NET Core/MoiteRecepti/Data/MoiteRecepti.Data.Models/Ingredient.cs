4c5b269dd59ee45bbe707b90aa74b93b760ee58b
AwakexD <aleks.parvanow@gmail.com>
2024-03-03 22:44:34 +0200

    Create db entities and migration

---
+++ ASP.NET Core/MoiteRecepti/Data/MoiteRecepti.Data.Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿namespace MoiteRecepti.Data.Models;
+
+using System.Collections.Generic;
+using MoiteRecepti.Data.Common.Models;
+
+public class Ingredient : BaseDeletableModel<int>
+{
+    public Ingredient()
+    {
+        this.Recipes = new HashSet<RecipeIngredient>();
+    }
+
+    public string Name { get; set; }
+
+    public ICollection<RecipeIngredient> Recipes { get; set; }
+}
